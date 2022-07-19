31bb478d7f0f22684823bcfce3b82cb14a55c3b5
zeina elkassas <zeinaahmed927@gmail.com>
2022-07-19 08:05:15 +0200

    updates

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-
+	"log"
+	"os"
 	"strings"
 ) 
 
@@ -11,25 +12,34 @@ import (
 
 const contentt = ` ; last modified 1 April 2001 by John Doe
 
-                [owner]
-                name = John Doe
-                organization = Acme Widgets Inc.
+                     [owner]
+                     name = John Doe
+                     organization = Acme Widgets Inc.
 
-				  ; use IP address in case network name resolution is not working
+				   ; use IP address in case network name resolution is not working
 
-                [database]   
-                server = 192.0.2.62     
-                port = 143
-                file = payroll.dat
+                     [database]   
+                    server = 192.0.2.62     
+                    port = 143
+                    file = payroll.dat
 
 			    `
 
 
+type Parser struct{
+	ini map[string]map[string]string
+}
 
+func (p *Parser)LoadFromFile(filename string) (str string){
 
-// func LoadFromFile(){
+	b, err := os.ReadFile(filename)
+	if err != nil {
+        fmt.Print(err)
+    }
+	x := string(b) 
 
-// }
+	return x
+}
 func Keys[M ~map[K]V, K comparable, V any](m M) []K {
     r := make([]K, 0, len(m))
     for k := range m {
@@ -58,62 +68,142 @@ func removeLinesContainingcomments(input string, toRemove []string) string {
   
 	return input
   }
+  func removeEmptyStrings(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
+}
+func (p *Parser) LoadFromString(content string)( ) {
 
-func LoadFromString(content string)( x map[string]map[string]string) {
+
+	p.ini=loadString(content)
+
+}
+func loadString(content string)( x map[string]map[string]string) {
     toRemove := []string{";"}
 	res := removeLinesContainingcomments(content,toRemove )
     
      println(res)
+	 rest := strings.Split(res," ") 
+    fmt.Println(rest)
+	 rest = removeEmptyStrings(rest)
+	 
 	 ress := make(map[string]map[string]string)
-	 z := json.Unmarshal([]byte(res), &ress)
+	 
+	 for i ,s := range rest{
+	  fmt.Println(i,s)
+	 }
+
+	 
+	
+	 for i, s := range rest {
+		fmt.Println(i, s)
+		result := []rune(s)
+		var sectionnn string
+		firstcharacter := string(result[0:1])
+
+		if firstcharacter == "["{
+			ress[s]=make(map[string]string)
+			sectionnn = s
+			
+			println(sectionnn)
+		}
+		fmt.Println(ress)
+		
+	
+		if rest[i+1] =="="{
+			
+			
+			//rest[i+1] = ress[sectionnn]
+			
+			
+			//ress[sectionnn][s]= rest[i+1]
+
+            println(rest[i-1])
+		}
+		
+		
+	}
+	 fmt.Println(ress)
+
 	 println("ssss")
-	 fmt.Println(z)
-	  fmt.Println(ress)
+	
 	
 	return ress
 }
 
 
-func GetSectionNames(content string)(arr []string){
+func (p *Parser)GetSectionNames()(arr []string){
 
-	mapp := LoadFromString(content)
-	strKeys := Keys(mapp)
+	
+	strKeys := Keys(p.ini)
 
    
   return strKeys
 }
 
-func GetSections(content string,name string)(sections map[string]string){
-	mapp := LoadFromString(content)
-	sections = mapp[name]
+func (p *Parser)GetSections()(sections map[string]map[string]string){
+	
+	sections = p.ini
      return sections
 
 }
-func Get(content string,section string,key string) (value string){
-	mapp := LoadFromString(content)
-    value = mapp[section][key]
+func (p *Parser) Get(section string,key string) (value string){
+	
+    value = p.ini[section][key]
 	return value
 
 }
 
-func Set(content string,section string,key string, value string){
-	mapp := LoadFromString(content)
+func (p *Parser) Set(section string,key string, value string){
+	
 
-	mapp[section][key] = value
-	SaveToFile()
+	p.ini[section][key] = value
+	
 
 }
 
-// func ToString(){
+func (p *Parser) ToString()(final string){
 
-// }
-func SaveToFile(){
+	jsonStr, err := json.Marshal(p.ini)
+	
+    if err != nil {
+        fmt.Printf("Error: %s", err.Error())
+    } else {
+        fmt.Println(string(jsonStr))
+    }
+	return string(jsonStr)
+}
 
+func (p *Parser) SaveToFile(finalstr string)(err error){
+	f, err := os.Create("output.txt")
+	if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
+	_, err2 := f.WriteString(finalstr)
+	if err2 != nil {
+        log.Fatal(err2)
+    }
+	return err
 }
 
 
 
 func main(){
 	
-	 fmt.Println(LoadFromString(contentt))
+	 fmt.Println(loadString(contentt))
+	 ds := Parser{}
+
+	ds.LoadFromFile("file.txt")
+
+	fmt.Println(ds.GetSections())
+
+	ds.GetSectionNames()
+
 }
+
